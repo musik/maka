7300e2735aa92ac6702ee6bbf27ac84b5ab7efe0
musik <cgicn@qq.com>
2014-06-01 16:05:52 +0800

    home news tab

--- skin/mv2/js/home.js
+++ skin/mv2/js/home.js
@@ -1,6 +1,7 @@
 $(function() {
   $(".f-tab-t").hover(function() {
-    $('.f-tab-c').hide()
+    gr = $(this).parent().parent()
+    gr.find('.f-tab-b').hide()
     $('.f-'+ $(this).attr('id')).show()
     $('.f-tab-t.current').removeClass("current")
     $(this).addClass("current")
